d7ece54e710e408555afcb2eba201831451fe1a3
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-04-01 18:18:39 -0300

    Update project

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -4,11 +4,20 @@ import { BurgerIngredient } from './BurgerIngredient/BurgerIngredient';
 import './Burger.css';
 
 export const Burger = (props: any) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((item, index) => {
+        return (
+          <BurgerIngredient key={igKey + index} type={igKey} />
+        );
+      })
+    });
+
+  console.log(transformedIngredients);
   return (
     <div className='Burger'>
       <BurgerIngredient type='bread-top'/>
-      <BurgerIngredient type='cheese'/>
-      <BurgerIngredient type='meat'/>
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom'/>
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Burger } from '../../components/Burger/Burger';
 
 
 export const BurgerBuilder = () => {
+  interface IBurguerIngredients {
+    ingredients: {
+      salad: number
+      bacon: number
+      cheese: number
+      meat: number
+    }
+  }
+
+  const [state, setState] = useState<IBurguerIngredients>(
+    {
+      ingredients: {
+        salad: 1,
+        bacon: 1,
+        cheese: 2,
+        meat: 2,
+      }
+    }
+  );
+
   return (
     <>
-      <Burger />
+      <Burger ingredients={state.ingredients}/>
       <div>Build Controls</div>
     </>
   );
